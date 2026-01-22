af27c1fc2e2f06e0b70e3ee81c2399fed1abba69
rytisp91 <rytazz@gmail.com>
2026-01-22 20:53:09 +0200

    Add Bootstrap and SweetAlert2

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,7 @@
 import './bootstrap';
+
+import 'bootstrap';
+import Swal from 'sweetalert2';
+
+window.Swal = Swal;
+
--- resources/views/admin/conferences/index.blade.php
+++ resources/views/admin/conferences/index.blade.php
@@ -5,5 +5,8 @@
 @section('content')
     <h1>{{__('ui.conference_management')}}</h1>
     <p>{{__('ui.conferences_list_placeholder')}}</p>
+    <button type="button" class="btn btn-primary" onclick="Swal.fire('OK', 'SweetAlert2 veikia', 'success')">
+        JS test
+    </button>
 @endsection
 
